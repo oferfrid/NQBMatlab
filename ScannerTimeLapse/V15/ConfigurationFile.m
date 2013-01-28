c728a91a58929532b49f97854363092b74215325
Irit Levin <Irit.le.re@gmail.com>
2013-01-28 16:11:45 +0200

    u
    Signed-off-by: Irit Levin <Irit.le.re@gmail.com>

--- ScannerTimeLapse/V15/ConfigurationFile.m
+++ ScannerTimeLapse/V15/ConfigurationFile.m
@@ -1,5 +1,5 @@
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\TL_Utils\TL_Utils_V1_5'
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\TL_Utils\TL_Utils_V1_5\motion'
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\ScannerTimeLapse\V15'
+addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DevelopGit\TL_Utils\TL_Utils_V1_5'
+addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DevelopGit\TL_Utils\TL_Utils_V1_5\motion'
+addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DevelopGit\ScannerTimeLapse\V15'
 
 
