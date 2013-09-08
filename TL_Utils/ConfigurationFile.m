fe6159e52096f077bec90a7945735a978582fedf
Ofer Fridman <oferfrid@gmail.com>
2013-09-08 17:46:38 +0300

    Delete ConfigurationFile.m

--- TL_Utils/ConfigurationFile.m
+++ TL_Utils/ConfigurationFile.m
@@ -1,5 +1,3 @@
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\TL_Utils\TL_Utils_V1_5'
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\TL_Utils\TL_Utils_V1_5\motion'
-addpath 'C:\Documents and Settings\Irit\My Documents\MATLAB\DEVELOP\ScannerTimeLapse\V15'
+
 
 
