0ff38eff75c027083a770265f6a648ca349de0a3
nirda <nirdick@gmail.com>
2014-07-13 13:30:11 +0300

    new ver

---
+++ TrackColonies/cleanImage.m
@@ -0,0 +1,20 @@
+function clnImg = cleanImage(I,bg)
+%% Img = cleanImg(I,bg)
+% --------------------------------------------------------------
+% purpose: reading a picture file and sharpening it
+%
+% Arguments: I   - A grayscale image
+% Returns:   Img - An image clean from noises
+% --------------------------------------------------------------
+% Irit Levin (with Alex) 8.8.06
+% --------------------------------------------------------------
+
+% disksize = 50;
+clnImg = rgb2gray(imsubtract(I,bg));
+
+%%
+% figure;
+% subplot(1, 2, 1); imshow(I*5); title('I');
+% subplot(1, 2, 2); imshow(background*5); title('background');
+% figure;
+% imshow(clnImg); title('clnImg');
