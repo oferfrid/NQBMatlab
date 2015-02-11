ff9169627b64f2f591024759d64df249f4d0be90
nirda <nirdick@gmail.com>
2015-02-11 11:05:27 +0200

    Update GetMask.m
    fix bug - re prepare delete mask

--- TrackColonies/GetMask.m
+++ TrackColonies/GetMask.m
@@ -3,13 +3,18 @@ function relevantArea = GetMask(Data,Rows,Cols)
         relevantArea = Data.FullMask;
     % Working with a circled area
     else
-        x=Data.CircleMask.X;
-        y=Data.CircleMask.Y;
-        r=Data.CircleMask.R;
+        if  isfield(Data,'CircleMask')
+            x=Data.CircleMask.X;
+            y=Data.CircleMask.Y;
+            r=Data.CircleMask.R;
+        else
+            x=Data.PlateCirc.X;
+            y=Data.PlateCirc.Y;
+            r=Data.PlateCirc.R;
+        end
         indMat(:,:,1) = repmat((1:Rows)', 1, Cols);
         indMat(:,:,2) = repmat( 1:Cols , Rows ,1);
         dist = sqrt((indMat(:,:,2)-x(1)).^2+(indMat(:,:,1)-y(1)).^2);
         relevantArea = dist<=r;
     end
 end
-
