cc910410cc11b106225872885932152030391fe9
nirda <nirdick@gmail.com>
2015-02-15 09:31:36 +0200

    Create SetStartingTime.m

---
+++ TrackColonies/SetStartingTime.m
@@ -0,0 +1,14 @@
+function [ output_args ] = SetStartingTime(SourceDir,StartingTime,LogFile)
+    if nargin>2
+        fid = fopen(LogFile);
+        firstLine=fgets(fid);
+        fclose(fid);
+
+        % get the time from the first line
+        [startIndex,endIndex] = regexpi(firstLine,...
+                                        '\d\d\d\d/\d\d/\d\d \d\d:\d\d:\d\d');
+        StartTime=firstLine(startIndex:endIndex);
+        StartingTime=datenum(StartTime,'yyyy/mm/dd HH:MM:SS');
+    end
+    save(fullfile(SourceDir,GetDefaultDataName),'StartingTime','-append');
+end
