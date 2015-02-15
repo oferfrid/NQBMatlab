5eb611b639e72039513b5e8ff46c301901c3cb41
nirda <nirdick@gmail.com>
2015-02-15 09:44:03 +0200

    Update getAppearanceTime.m
    add log file option in arguments

--- ConditionAnalysis/getAppearanceTime.m
+++ ConditionAnalysis/getAppearanceTime.m
@@ -1,30 +1,55 @@
-function [id,AppearanceTime ] = getAppearanceTime( SourceDirs )
-    
+function [ApperanceTime] = getAppearanceTime(SourceDirs,DataTimeFlag,BeginTimes)
+
+    substract=0;
+    sentTimeFlag=0;
+    if nargin < 2
+        DataTimeFlag=false;
+    else
+        substract=1;
+        
+        if nargin > 2
+            sentTimeFlag=1;
+            if(~iscell(BeginTimes))
+                BeginTimes = {BeginTimes};
+            end
+        end
+    end
+
     if(~iscell(SourceDirs))
         SourceDirs = {SourceDirs};
     end
     
     numOfSources=length(SourceDirs);
     
-    id=cell(numOfSources);
-    AppearanceTime=cell(numOfSources,1);
+    ApperanceTime.plate=[];
+    ApperanceTime.id=[];
+    ApperanceTime.time=[];
     
     for i=1:numOfSources
         % Load data file
         data=load(fullfile(SourceDirs{i},GetDefaultDataName));
         
+        if isfield(data,'StartingTime') && DataTimeFlag
+            currBeginTime=data.StartingTime;
+        elseif sentTimeFlag
+            currBeginTime=BeginTimes{i};
+        else
+            substract=0;
+        end
+        
         indexes = 1:length(data.IgnoredColonies);
         indexes = indexes((~data.IgnoredColonies)&(data.Area(end,:)>0)');
-        id{i}=indexes;
         
-        numOfColonies=length(indexes);
-        I=[];
-        for j=1:numOfColonies
-            % Get the last zero
-            lastZeroIdx=find(data.Area(:,indexes(j))==0,1,'last');
-            I=[I,lastZeroIdx+1];
-        end
+        [~,I]=max(data.Area(:,indexes)>0,[],1);
         PlateAppearanceTime = data.FilesDateTime(I);
-        AppearanceTime{i}=PlateAppearanceTime;
+        
+        if substract
+            PlateAppearanceTime=round((PlateAppearanceTime-currBeginTime)*24*60);
+        end
+               
+        ApperanceTime.plate=[ApperanceTime.plate,i*ones(size(indexes))];
+        ApperanceTime.id=[ApperanceTime.id,indexes];
+        ApperanceTime.time=[ApperanceTime.time,PlateAppearanceTime];
     end
 end
+
