4fff34130fdb9570c61a1fe6717a9443e550af35
nirda <nirdick@gmail.com>
2015-08-03 11:13:11 +0300

    Create getDistrStatistics.m

---
+++ ConditionAnalysis/getDistrStatistics.m
@@ -0,0 +1,25 @@
+function [Stat] = getDistrStatistics(TimeAxis, Distr)
+% Stat = ExperimentStatistics(TimeAxis, Distr)
+% -------------------------------------------------------------------------
+% Purpose: checking statistical parmeters for a histogram
+% Arguments: TimeAxis - Time
+%       Distr - appearance time for each bacteria (not a histogram)
+% Returns: Stat.total
+%              .Avg
+%              .std
+%              .skw
+%              .max
+%              .median
+%              .stdMed
+% -------------------------------------------------------------------------
+% Irit Levin Reisman. 2015
+    Stat.total = length(Distr);
+    Stat.Avg   = mean(Distr);
+    Stat.std   = std(Distr);
+    Stat.skw   = skewness(Distr);
+    h          = hist(Distr, TimeAxis);
+    [val, Ind] = max(h);
+    Stat.max   = TimeAxis(Ind);
+    Stat.median= median(Distr);
+    Stat.stdMed= sqrt(sum((Distr - Stat.median).^2)/Stat.total);
+end
