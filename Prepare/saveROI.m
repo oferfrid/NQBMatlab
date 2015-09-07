61ba12b39e7226e728ea8791a9ff971bcd9fd90c
nirda <nirdick@gmail.com>
2015-09-07 10:40:48 +0300

    Update saveROI.m

--- Prepare/saveROI.m
+++ Prepare/saveROI.m
@@ -2,7 +2,18 @@ function [destNames]=saveROI(Image,DestDirNames,ImageName,...
                              Plates2Cut,index,limits,rects)
 % Save the roi for of each plate as long as limits are not being
 % exceeded. Note that each plate has its own limits.
-%Nir Dick 2015
+% arguments:
+%     Image - the scanner's image to cut
+%     DestDirNames - the location for saving the roi
+%     ImageName - the name of the scanners image
+%     Plates2Cut - relevant plates
+%     index - undex of current scanner image
+%     limits - the last index already being calculated for each plate.
+%             if index is below plate's limit no roi is being saved.
+%     rects - position data of roi
+% return value:
+%     destsNames - names array of saved roi
+% Nir Dick 2015
 
     numOfDests=length(DestDirNames);
     
