5207a4f0dfaf8669753ccf979b170d1693f591a3
nirda <nirdick@gmail.com>
2015-09-07 10:19:47 +0300

    Update GetDataName.m

--- Prepare/GetDataName.m
+++ Prepare/GetDataName.m
@@ -1,4 +1,7 @@
 function name = GetDataName(SourceDir)
+% name = GetDataName(SourceDir)
+% Calculate the expected data name of sent directory
+% SourceDir - The directory
     [~,dir,~]=fileparts(SourceDir);
     name=[dir '_data.mat'];
     name=fullfile(SourceDir,name);
