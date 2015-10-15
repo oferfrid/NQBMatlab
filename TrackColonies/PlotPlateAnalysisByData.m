0d63be50ff700ba37b5b8c80eab4b1c5606c5cd3
nirda <nirdick@gmail.com>
2015-10-15 12:22:25 +0300

    Update PlotPlateAnalysisByData.m

--- TrackColonies/PlotPlateAnalysisByData.m
+++ TrackColonies/PlotPlateAnalysisByData.m
@@ -1,6 +1,22 @@
 function himage = PlotPlateAnalysisByData(...
                                     DirName,FileName,handle,Limits,TH,...
                                     Background,Mask,Lrgb,Title,forMovie)
+%himage = PlotPlateAnalysisByData(DirName,FileName,handle,Limits,TH,...
+%                                 Background,Mask,Lrgb,Title,forMovie)
+% Plot the plate image of current time by data sent as arguments.
+% arguments:
+% DirName - Name of directory
+% FileName - Name of the image to analyse and plot
+% handle - the handle to plot in
+% Limits - stretching limits (by it we proccessed the images)
+% TH - treshold for the grayscale to bw image
+% Background - the first image
+% Mask - binary image of the area
+% Lrgb - last Limage with color data
+% Title - title
+% forMovie - yes/no
+% Nir Dick 2015
+
    if nargin<10
        forMovie=false;
    end
