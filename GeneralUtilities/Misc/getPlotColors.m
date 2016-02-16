834547b16378c172ad1e117dcab9f271ce558a15
Irit Levin <irit.le.re@gmail.com>
2016-02-16 18:36:46 +0200

    adding colormap as an optional parameter

--- GeneralUtilities/Misc/getPlotColors.m
+++ GeneralUtilities/Misc/getPlotColors.m
@@ -1,14 +1,17 @@
-function color =getPlotColors(NumberOfColors,ind)
+function color =getPlotColors(NumberOfColors,ind, colorpmap_)
 %function color =getPlotColors(NumberOfColors,ind)
 
+y=jet;
+if nargin==3
+    y=colormap(colorpmap_);
+end
 
 %r= (ind-1)/NumberOfColors;
        %g=abs((ind-1)/NumberOfColors*2 -1);
        %b=1 - (ind-1)/NumberOfColors;
        %color = [ r g b];
-       
-       
-       y=jet;
-       colorind = round((ind-1)/(NumberOfColors-1)*63 +1);
+
+%        colorind = round((ind-1)/(NumberOfColors-1)*63 +1);
+       colorind = round((ind-1)/(NumberOfColors-1)*(size(y,1)-1) +1);
        color = y(colorind,:);
 end
